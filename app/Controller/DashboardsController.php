f4ab77a73eaf83d215b0a0ce3b684805972ecd9f
devasish <dev.achieve@gmail.com>
2015-03-15 19:40:46 +0530

    Route Updated.

---
+++ app/Controller/DashboardsController.php
@@ -0,0 +1,17 @@
+<?php
+
+App::uses('AppController', 'Controller');
+
+/**
+ * Users Controller
+ *
+ * @property User $User
+ * @property PaginatorComponent $Paginator
+ */
+class DashboardsController extends AppController {
+
+    public function display() {
+        
+    }
+
+}
